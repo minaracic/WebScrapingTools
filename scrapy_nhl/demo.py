f4c161efd3eee3cc98dc16d8140875cbf4b13410
Mina Racic <racicmina@gmail.com>
2020-09-13 17:08:26 +0200

    Added avg time and cpu usage

--- scrapy_nhl/demo.py
+++ scrapy_nhl/demo.py
@@ -4,41 +4,65 @@ import time
 import logging
 import psutil
 import os
+import threading 
+from scrapy.exporters import CsvItemExporter
+from scrapy.crawler import CrawlerProcess
+import datetime
+# from scrapy.statscollectors import StatsCollector
 
-logger = logging.getLogger('mycustomlogger')
-start_time = 0
-end_time = 0
 
-pid = os.getpid()
-this_thread = psutil.Process(pid)
-start_cpu_usage = psutil.cpu_percent(None)
+proc = psutil.Process()
+start_cpu_usage = psutil.cpu_percent(interval=1)
+# end_time = 0
+# start_time = 0
+total_time = 0
+total_time2 = 0
 
-class QuotesSpider(scrapy.Spider):
-    start_time = time.time()
+class TeamsSpider1(scrapy.Spider):
     name = 'hockey_teams'
+    start_urls = []
 
-    start_urls = [
-        'https://scrapethissite.com/pages/forms/'
-    ]
+    start_time = time.time()
+    end_time = 0
 
-    for i in range(2, 25):
+    for i in range(1, 25):
         url = 'https://scrapethissite.com/pages/forms/?page_num=%s' % i
         start_urls.append(url)
 
+    # print("CURRENT CPU >>> %i" %(this_thread.cpu_num()))
     def parse(self, response):
         for team in response.css('.team'):
+            
             yield {
-                'Team': team.css('.name::text').extract_first(),
-                'Wins': team.css('.wins::text').extract_first(),
+                'Team': team.css('.name::text').extract_first().strip(),
+                'Wins': team.css('.wins::text').extract_first().strip(),
             }
 
     def closed(self, reason):
-        end_cpu_usage = psutil.cpu_percent(None)
-        logger.info( 'Current CPU usage >>> %f' % end_cpu_usage)
-        memory_used = this_thread.memory_info()[0]/10**6
-        logger.info("Memory used >>> %f" %memory_used)
+        end_time = time.time()
+        end_cpu_usage = psutil.cpu_percent(None,percpu=True)
+        self.logger.info("CPU usage >>>> %f" %end_cpu_usage[0])
+        self.logger.info("CPU usage >>>> %f" %end_cpu_usage[1])
+        self.logger.info("CPU usage >>>> %f" %end_cpu_usage[2])
+        self.logger.info("CPU usage >>>> %f" %end_cpu_usage[3])
+        print("TotalTime >>>> ") 
+        
+        global total_time 
+        total_time += end_time - self.start_time
+        print(total_time)
 
-    
+avg_elapsed_time = 0
 
+for i in range(0, 1):
+    process = CrawlerProcess(settings={
+        "FEEDS": {
+            "teams.csv": {"format": "csv"},
+        },
+        "LOG_LEVEL": 'INFO'
+    })
 
+    process.crawl(TeamsSpider1)
+process.start()
 
+print("AVG time2 %f"%( total_time2/3))
+print("AVG time1 %f"%( total_time/5))
